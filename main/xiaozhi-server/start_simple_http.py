1fcf29351b9345279da702db00195d9c51926931
砂糖橘 <54745033+Foxerine@users.noreply.github.com>
2025-08-14 14:04:32 +0800

    增加启动测试页面功能
    增加启动测试页面功能

---
+++ main/xiaozhi-server/start_simple_http.py
@@ -0,0 +1,22 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8005  # 你可以修改端口号
+DIRECTORY = "./test"  # 指定要对外服务的目录
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        # 更改当前工作目录到指定目录
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("0.0.0.0", PORT), MyHandler) as httpd:
+    print(f"serving files from {DIRECTORY} at port {PORT}")
+    print("Server is running. Press Ctrl+C to stop the server.")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print("Server stopped.")
+    httpd.server_close()
+    print("Server closed.")
